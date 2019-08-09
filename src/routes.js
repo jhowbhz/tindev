648f20143fc9035e3054eeb7ec3a30c8953ee29d
Jhon DEV <jhowjhoe@icloud.com>
2019-08-09 00:32:56 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const routes = express.Router();
 
 // get, post, put, delete
-routes.get('/', (requisicao, resposta) => {
+routes.get('/api/', (requisicao, resposta) => {
     return resposta.json({ mensagem: `Hello World xxxxxxxxxxxxx ${requisicao.query.name}`});
 });
 
-routes.post('/devs', (requisicao, resposta) => {
+routes.post('/api/devs', (requisicao, resposta) => {
 
     console.log(requisicao.body);
     return resposta.json(requisicao.body);
